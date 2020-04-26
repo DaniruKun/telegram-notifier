d2a383edb2886d57921294cf936ef41d88e139de
dpetrovs <dpetrovs@evolutiongaming.com>
2020-04-26 17:34:22 +0300

    Bump lein ver

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject telegram-notifier "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
